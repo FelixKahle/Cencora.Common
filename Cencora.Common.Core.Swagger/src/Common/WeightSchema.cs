db50c7499e04f2643c801d7dd0e03cba3a2107b3
Felix Kahle <felix.kahle21@gmail.com>
2024-06-07 11:05:11 +0200

    Add Swagger

---
+++ Cencora.Common.Core.Swagger/src/Common/WeightSchema.cs
@@ -0,0 +1,22 @@
+// Copyright 2024 Cencora. All rights reserved.
+//
+// Written by Felix Kahle, A123234, felix.kahle@worldcourier.de
+
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Cencora.Common.Core.Swagger
+{
+    public class WeightSchema : ISchemaFilter
+    {
+        public void Apply(OpenApiSchema schema, SchemaFilterContext context)
+        {
+            if (context.Type == typeof(Weight))
+            {
+                schema.Type = "object";
+                schema.Properties.Add("value", new OpenApiSchema { Type = "number", Format = "double" });
+                schema.Properties.Add("unit", new OpenApiSchema { Type = "string" });
+            }
+        }
+    }
+}
