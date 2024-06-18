d1bc729848a98e36b87ca1ff251d3361e8b8734a
Felix Kahle <felix.kahle21@gmail.com>
2024-06-18 19:16:27 +0200

    Add IsUnique and IsUniqueBy

--- Cencora.Common.Core/src/Extensions/ListExtensions.cs
+++ Cencora.Common.Core/src/Extensions/ListExtensions.cs
@@ -20,5 +20,29 @@ namespace Cencora.Common.Core
         {
             return index >= 0 && index < list.Count;
         }
+
+        /// <summary>
+        /// Determines whether the elements in the list are unique.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the list.</typeparam>
+        /// <param name="list">The list to check for uniqueness.</param>
+        /// <returns><c>true</c> if all elements in the list are unique; otherwise, <c>false</c>.</returns>
+        public static bool IsUnique<T>(this IList<T> list)
+        {
+            return list.Distinct().Count() == list.Count;
+        }
+
+        /// <summary>
+        /// Determines whether the elements in a list are unique based on a specified key.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the list.</typeparam>
+        /// <typeparam name="TKey">The type of the key used for comparison.</typeparam>
+        /// <param name="list">The list to check for uniqueness.</param>
+        /// <param name="keySelector">A function to extract the key from each element.</param>
+        /// <returns><c>true</c> if the elements in the list are unique based on the specified key; otherwise, <c>false</c>.</returns>
+        public static bool IsUniqueBy<T, TKey>(this IList<T> list, Func<T, TKey> keySelector)
+        {
+            return list.DistinctBy(keySelector).Count() == list.Count;
+        }
     }
 }
